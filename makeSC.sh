1870178d701080693daa8f2c9e6d1b07400c6a99
Jeff Vroom <jeff@jvroom.com>
2014-09-24 22:06:30 -0700

    update

--- makeSC.sh
+++ makeSC.sh
@@ -19,8 +19,8 @@ echo "Building StrataCode into: $BUILDDIR"
 
 # For a clean build you should run this from the source directly, or first build StrataCode, then use it to build itself.
 mkdir -p $BUILDDIR
-sc -c -a coreRuntime -d $BUILDDIR
-sc -c -a system -d $BUILDDIR
+sc -c -a coreRuntime -da $BUILDDIR
+sc -c -a system -da $BUILDDIR
 
 # Now we package up the STAGEDIR/StrataCode directory.  
 rm -rf STAGEDIR/StrataCode
