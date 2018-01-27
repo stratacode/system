3305ffd19d0c252b359121ec09924af18f48412b
Jeff Vroom <jeff@jvroom.com>
2018-01-27 15:42:09 -0800

    moved IChildInit to scdyn.js

--- coreRuntime/src/sc/obj/IChildInit.java
+++ coreRuntime/src/sc/obj/IChildInit.java
@@ -7,7 +7,7 @@ package sc.obj;
 /**
  * Called just before we get the children - used as a hook for frameworks that might lazily init children, like the SCHTML repeat tags.
  */
-@sc.js.JSSettings(jsModuleFile="js/scgen.js", prefixAlias="sc_")
+@sc.js.JSSettings(jsLibFiles="js/scdyn.js", prefixAlias="sc_")
 public interface IChildInit {
    void initChildren();
 }
