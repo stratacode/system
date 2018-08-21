b796b2abcd7a86ea41c6b259691b903f7a3eedf6
Jeff Vroom <jeff@jvroom.com>
2018-08-21 12:01:18 -0700

    fixed regression with UnknownReferredType change
    for transformed models, reresolving with a null type can clear out
    info we need to do the data binding transforms

--- system/src/sc/lang/java/IdentifierExpression.java
+++ system/src/sc/lang/java/IdentifierExpression.java
@@ -832,12 +832,15 @@ public class IdentifierExpression extends ArgumentsExpression {
          Object boundType = boundTypes == null ? null : boundTypes[boundTypes.length - 1];
          // A method which we could not resolve, so propagateInferredTypes never ran for the args - let them know there will not be an inferred type so we flush out suppressed errors
          if (boundType == null || errorArgs != null) { // TODO: should we add more state here to differentiate between
-            for (Expression argExpr: arguments) {
-               Object exprType = argExpr.getGenericType();
-               if (exprType == null)
-                  exprType = UnknownReferredType;
-               // else - might as well use the exprType in this case?
-               argExpr.setInferredType(exprType, true);
+            JavaModel model = getJavaModel();
+            if (!model.disableTypeErrors) { // During transform, we will have turned off type errors for expressions to JS methods and stuff - the resolve here will break those cases
+               for (Expression argExpr: arguments) {
+                  Object exprType = argExpr.getGenericType();
+                  if (exprType == null)
+                     exprType = UnknownReferredType;
+                  // else - might as well use the exprType in this case?
+                  argExpr.setInferredType(exprType, true);
+               }
             }
          }
       }
