d221bef8952c1ba5e60c4abf0b2c1da31399d02a
Jeff Vroom <jeff@jvroom.com>
2018-10-17 20:46:59 -0700

    fixed message

--- system/src/sc/layer/LayeredSystem.java
+++ system/src/sc/layer/LayeredSystem.java
@@ -3632,7 +3632,7 @@ public class LayeredSystem implements LayerConstants, INameContext, IRDynamicSys
             if (options.testScriptName != null && options.testMode && !sys.peerMode) {
                String pathName = options.testScriptName;
                SrcEntry layerFile = sys.buildLayer.getLayerFileFromRelName(pathName, true, true);
-               System.out.println("Running test script: " + pathName + " from: " + sys.buildDir + " script layer: " + layerFile == null ? "null" : layerFile.layer);
+               System.out.println("Running test script: " + pathName + " from: " + sys.buildDir + " script layer: " + (layerFile == null ? "null" : layerFile.layer));
                Layer includeLayer = layerFile == null ? null : layerFile.layer;
                if (options.includeTestScript)
                   sys.cmd.pushIncludeScript(sys.buildDir, pathName, includeLayer);
