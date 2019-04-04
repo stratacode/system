3fbd8f8ec2434f47285874f4ee8f6d351009d5f8
Jeff Vroom <jeff@jvroom.com>
2019-04-04 14:22:03 -0700

    typo

--- system/src/sc/lang/java/IdentifierExpression.java
+++ system/src/sc/lang/java/IdentifierExpression.java
@@ -1173,7 +1173,7 @@ public class IdentifierExpression extends ArgumentsExpression {
     * Utility method for IdentifierExpr and SelectExpr to apply the data binding contract to a given property of the
     * referenced object.   When referenceOnly = true, this is a getX reference only - not requiring that the referred
     * property be made bindable.  This method both ensures the property is registered with it's parent type and
-    * adds a reference to that type in ReverseDependencies - reflecting the depenpendcy from the type which needs
+    * adds a reference to that type in ReverseDependencies - reflecting the dependency from the type which needs
     * this property bindable.  When checkAnnotations is true, referenceOnly starts out false but then turns to 'true' if
     * this property is marked as @Bindable so that we inject it during the code-gen phase (i.e. not manual bindable).
     */
