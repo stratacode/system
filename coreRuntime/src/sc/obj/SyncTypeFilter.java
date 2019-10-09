d17addc20690d7e030b9286c762433fb663736b5
Jeff Vroom <jeff@jvroom.com>
2019-10-08 19:08:14 -0700

    runtime mode for this annotation so we can use it for compiled classes

--- coreRuntime/src/sc/obj/SyncTypeFilter.java
+++ coreRuntime/src/sc/obj/SyncTypeFilter.java
@@ -1,9 +1,16 @@
 package sc.obj;
 
-/** 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
  * Used to include additional type names in the "syncTypeFilter", used
  * by servers to determine which types each client can access.
  */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
 public @interface SyncTypeFilter {
    String[] typeNames();
 }
