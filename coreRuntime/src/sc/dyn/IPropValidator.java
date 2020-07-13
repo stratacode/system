2a201053d8efaaa63cdcdbe58b3ea955780bfce4
Jeff Vroom <jeff@jvroom.com>
2020-07-13 12:51:03 -0700

    added default methods for property validation

--- coreRuntime/src/sc/dyn/IPropValidator.java
+++ coreRuntime/src/sc/dyn/IPropValidator.java
@@ -1,6 +1,7 @@
 package sc.dyn;
 
 import java.util.Map;
+import java.util.TreeMap;
 
 /** 
  * Implemented by components that manage property errors. For DB components, when properties 
@@ -9,4 +10,26 @@ import java.util.Map;
 public interface IPropValidator {
    Map<String,String> getPropErrors();
    void setPropErrors(Map<String,String> errors);
+
+   default void addPropError(String propName, String errorMessage) {
+      Map<String,String> propErrors = getPropErrors();
+      boolean doSet = false;
+      if (propErrors == null) {
+         propErrors = new TreeMap<String,String>();
+         doSet = true;
+      }
+      propErrors.put(propName, errorMessage);
+      if (doSet)
+         setPropErrors(propErrors);
+   }
+
+   default void removePropError(String propName) {
+      Map<String,String> propErrors = getPropErrors();
+      boolean doSet = false;
+      if (propErrors == null)
+         return;
+      propErrors.remove(propName);
+      if (propErrors.size() == 0)
+         setPropErrors(null);
+   }
 }
