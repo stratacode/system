2139234f7ed840cf17c04849093df34ed350e65d
Jeff Vroom <jeff@jvroom.com>
2020-11-03 20:46:33 -0800

    added comment

--- coreRuntime/src/sc/bind/IBindable.java
+++ coreRuntime/src/sc/bind/IBindable.java
@@ -4,8 +4,9 @@
 
 package sc.bind;
 
-public interface IBindable
-{
+// TODO: remove this interface? It's a placeholder to support storing the listeners for properties on an object
+// without using the WeakHashMap
+public interface IBindable {
    BindingListener[] getBindingListeners();
    void setBindingListeners(BindingListener[] bindings);
 }
